f89df86e53fbad43117a8dd7bc0c69858e4a9a08
Daniel Moreno <daniel.moreno@creativaconsultores.com>
2022-07-26 23:09:15 -0600

    Real change

--- app/src/main/java/com/example/ketchup/MainActivity.java
+++ app/src/main/java/com/example/ketchup/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // thi will be a test
+        // this will be a test
     }
 }
